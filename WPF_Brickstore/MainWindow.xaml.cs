946adf71731e204f01e0aba6fef98f6b448dc08a
Nagy Martin Márk <nagy.martin.mark@burg.lo.hu>
2024-09-06 10:47:06 +0200

    Done

---
+++ WPF_Brickstore/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WPF_Brickstore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static ObservableCollection<Piece> pieces = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            loadFiles();
+        }
+
+        public void loadFiles()
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    XDocument bsx = XDocument.Load(openFileDialog.FileName);
+                    foreach (var elem in bsx.Descendants("Item"))
+                    {
+                        string id = elem.Element("ItemID").Value;
+                        string name = elem.Element("ItemName").Value;
+                        string category = elem.Element("CategoryName").Value;
+                        string color = elem.Element("ColorName").Value;
+                        int quantity = Convert.ToInt32(elem.Element("Qty").Value);
+                        Piece newPiece = new Piece(id, name, category, color, quantity);
+                        pieces.Add(newPiece);
+                    }
+                    dgDisplay.ItemsSource = pieces;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
+            }
+        }
+
+        private void txtSearchbar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtSearchbar.Text != "")
+            {
+                ObservableCollection<Piece> searchedPieces = new();
+                dgDisplay.ItemsSource = searchedPieces;
+                if (Char.IsLetter(txtSearchbar.Text[0]))
+                {
+                    foreach (var item in pieces)
+                    {
+                        if (item.Name.ToLower().Contains(txtSearchbar.Text.ToLower()) || item.Category.ToLower().Contains(txtSearchbar.Text.ToLower()))
+                        {
+                            searchedPieces.Add(item);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var item in pieces)
+                    {
+                        if (item.Id.ToLower().Contains(txtSearchbar.Text.ToLower()))
+                        {
+                            searchedPieces.Add(item);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                dgDisplay.ItemsSource = pieces;
+            }
+        }
+    }
+}
---
+++ WPF_Brickstore/Piece.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Brickstore
+{
+    internal class Piece
+    {
+        string id;
+        string name;
+        string category;
+        string color;
+        int quantity;
+
+        public Piece(string id, string name, string category, string color, int quantity)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Category = category;
+            this.Color = color;
+            this.Quantity = quantity;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Category { get => category; set => category = value; }
+        public string Color { get => color; set => color = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+    }
+}
